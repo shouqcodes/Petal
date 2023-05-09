2808b8cbb2b881ba06440d6df7ba606624be8d48
shouqcodes <sshouqalotaibi@gmail.com>
2023-05-09 20:12:25 +0300

    Created and tested RegisterActivity.

--- app/src/main/java/com/example/petal/MainActivity.java
+++ app/src/main/java/com/example/petal/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.petal;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
